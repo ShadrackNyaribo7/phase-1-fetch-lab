c5062e805fb972191476d429a181d6b18f3346e7
shadracknyaribo <stilespulsar77@gmail.com>
2022-11-29 10:37:42 -0800

    done

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+//returned
+  .then((json) => console.log(json));
   
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+ //returned
+  .then((json) => console.log(json));
+  
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
+
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
